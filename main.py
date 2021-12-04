2e7c4182ee29a8ef0e6dc62061784b957a4ebcd0
InfiniteLoop <44796943+InfiniteLoopGameDev@users.noreply.github.com>
2021-12-04 08:41:58 +0000

    Added script parameter support

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-day = input("Input day number: ")
-location = "Day{}/module.py".format(day)
+import sys
 
+arg = sys.argv
+arg.pop(0)
+
+if len(arg) == 1:
+    day = str(arg[0])
+elif len(arg) > 1:
+    print("Only one argument needed")
+    sys.exit()
+else:
+    day = input("Input day number: ")
+
+location = "Day{}/module.py".format(day)
 exec(open(location).read())
