2c79230efb31d51f2c0b5cbe90290f7b26d55d32
InfiniteLoop <44796943+InfiniteLoopGameDev@users.noreply.github.com>
2021-12-06 18:13:53 +0000

    Day 6 Part 2
    Day 6 now uses the number of fish in each stage, instead of calculating them individually. Also fixed Day 5 indents.

--- Day6/module.py
+++ Day6/module.py
@@ -4,17 +4,25 @@ lines = f.read()
 dataStr = lines.split(",")
 data = []
 for i in dataStr:
-  data.append(int(i))
-count = 0
+    data.append(int(i))
 
-for e in range(80):
-  count += 1
-  print(data)
-  for i in range(0, len(data)):
-    if int(data[i]) == 0:
-      data[i] = 6
-      data.append(8)
-    else:
-      data[i] -= 1
+fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-print(len(data))
+for i in data:
+    fishes[i] += 1
+
+for i in range(256):
+    temp = fishes[0]
+    fishes[0] = fishes[1]
+    fishes[1] = fishes[2]
+    fishes[2] = fishes[3]
+    fishes[3] = fishes[4]
+    fishes[4] = fishes[5]
+    fishes[5] = fishes[6]
+    fishes[6] = fishes[7] + temp
+    fishes[7] = fishes[8]
+    fishes[8] = temp
+    if i == 79:
+        print("80 days: {}".format(sum(fishes)))
+
+print("256 days: {}".format(sum(fishes)))
