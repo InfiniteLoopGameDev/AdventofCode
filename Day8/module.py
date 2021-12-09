5861068da098219d8e861fe2f256bb2f32390289
InfiniteLoop <44796943+InfiniteLoopGameDev@users.noreply.github.com>
2021-12-09 08:04:33 +0000

    Day 8 Part 1.5

--- Day8/module.py
+++ Day8/module.py
@@ -1,7 +1,61 @@
+import re
+import math
+
 f = open("Day8/data")
 
 lines = f.read()
 dataStr = lines.split("\n")
+dataStr.pop(-1)
+dataList = []
 data = []
+# for i in dataStr:
+#     dataList.append(i.split("|")[1])
 for i in dataStr:
-    data.append(int(i))
+    data = data + i.split(" ")
+
+# data.pop(0)
+segmentsCipher = []
+segmentsDecode = []
+
+near = False
+count = 0
+temp = []
+for i in data:
+    if i != "|":
+        temp.append(i)
+        if near:
+            count += 1
+    if i == "|":
+        temp.append(i)
+        near = True
+    if count == 4:
+        segmentsCipher.append(temp)
+        temp = []
+        near = False
+        count = 0
+
+value = ""
+temp2 = []
+for i in segmentsCipher:
+    for j in i:
+        if len(j) == 2:
+            value += "1"
+        elif len(j) == 3:
+            value += "7"
+        elif len(j) == 4:
+            value += "4"
+        elif len(j) == 7:
+            value += "8"
+        elif j == "|":
+            temp2.append(value)
+            value = ""
+        else:
+            value += "."
+    temp2.append(value)
+    temp.append(temp2)
+    temp2 = []
+    for j in i:
+        if len(j) == 5:
+            help
+
+print(temp)
